18e1b5d843a05bd03a98427a355953bab86b5c8f
Slavomir <gagliardetto@users.noreply.github.com>
2021-03-07 01:35:48 +0100

    Initial commit

--- go.mod
+++ go.mod
@@ -1,8 +1,13 @@
-module github.com/gagliardetto/goimps
+module github.com/gagliardetto/depnet
 
 go 1.16
 
 require (
 	github.com/PuerkitoBio/goquery v1.6.1
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
+	github.com/davecgh/go-spew v1.1.1
+	github.com/gagliardetto/request v0.8.1-0.20190815080712-c8d37984afc2
 	github.com/gagliardetto/utilz v0.0.0-20210206133039-f73d84130f65
+	github.com/stretchr/testify v1.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,26 +1,48 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
+	"net/url"
 	"os"
+	"regexp"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gagliardetto/request"
 	. "github.com/gagliardetto/utilz"
 )
 
+// TODO:
+// - Input repo path
+// - Validate repo path
+// - Load first page for REPOSITORY (if not specified).
+// - Ask REPOSITORY or PACKAGE depdendents ? (show counts for both) (ask if not configured in a flag).
+// - If there are more than one package specified, ask which to choose (if flag is not set on a precise choice).
+// - Iterate and add to array.
+
+// countCleaner is used to remove anything that is NOT a number.
+var countCleaner = regexp.MustCompile("[^0-9]+")
+
 func main() {
 
-	if false { // Repos:
+	if true { // Repos:
 		if true {
 			// Request the HTML page.
 			res, err := os.Open(os.ExpandEnv("$GOPATH/src/github.com/gagliardetto/depnet/test-data/REPOSITORY.html"))
 			if err != nil {
 				log.Fatal(err)
 			}
-			deps := getDependantRepos(res)
+			deps, err := ExtractDependentsFromReader(res)
+			if err != nil {
+				panic(err)
+			}
 			for _, v := range deps {
 				Ln(v)
 			}
@@ -34,14 +56,19 @@ func main() {
 			if res.StatusCode != 200 {
 				log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 			}
-			deps := getDependantRepos(res.Body)
+			deps, err := ExtractDependentsFromReader(res.Body)
+			if err != nil {
+				panic(err)
+			}
 			for _, v := range deps {
 				Ln(v)
 			}
 		}
 	}
-	// Packages:
 
+	Ln(strings.Repeat("-", 50))
+
+	// Packages:
 	if true {
 		if true {
 			// Request the HTML page.
@@ -49,7 +76,10 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
-			deps := getDependantPackages(res)
+			deps, err := ExtractDependentsFromReader(res)
+			if err != nil {
+				panic(err)
+			}
 			for _, v := range deps {
 				Ln(v)
 			}
@@ -63,22 +93,311 @@ func main() {
 			if res.StatusCode != 200 {
 				log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 			}
-			deps := getDependantPackages(res.Body)
+			deps, err := ExtractDependentsFromReader(res.Body)
+			if err != nil {
+				panic(err)
+			}
 			for _, v := range deps {
 				Ln(v)
 			}
 		}
 	}
+
+	{
+		info, err :=
+			NewLoader("eslint/eslint").
+				Type(TYPE_REPOSITORY).
+				GetInfo()
+		if err != nil {
+			panic(err)
+		}
+		spew.Dump(info)
+	}
+	{
+		err :=
+			NewLoader("eslint/eslint").
+				Type(TYPE_REPOSITORY).
+				DoWithCallback(func(dep string) bool {
+					Ln(dep)
+					return true
+				})
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
-func getDependantRepos(reader io.Reader) []string {
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(reader)
-	if err != nil {
-		log.Fatal(err)
+var (
+	DefaultMaxIdleConnsPerHost = 50
+	Timeout                    = 5 * time.Minute
+	DefaultKeepAlive           = 180 * time.Second
+)
+
+var (
+	httpClient = NewHTTP()
+)
+
+func NewHTTPTransport() *http.Transport {
+	return &http.Transport{
+		IdleConnTimeout:     Timeout,
+		MaxIdleConnsPerHost: DefaultMaxIdleConnsPerHost,
+		Proxy:               http.ProxyFromEnvironment,
+		Dial: (&net.Dialer{
+			Timeout:   Timeout,
+			KeepAlive: DefaultKeepAlive,
+		}).Dial,
+		//TLSClientConfig: &tls.Config{
+		//	InsecureSkipVerify: conf.InsecureSkipVerify,
+		//},
+	}
+}
+
+// NewHTTP returns a new Client from the provided config.
+// Client is safe for concurrent use by multiple goroutines.
+func NewHTTP() *http.Client {
+	tr := NewHTTPTransport()
+
+	return &http.Client{
+		Timeout:   Timeout,
+		Transport: tr,
+	}
+}
+
+type Loader struct {
+	repoName string
+	owner    string
+
+	dependentType string
+	subPackage    string
+}
+
+func NewLoader(repo string) *Loader {
+	if repo == "" {
+		panic("repo not set")
 	}
 
-	var rawDependants []string
+	repo = strings.TrimSpace(repo)
+	repo = strings.TrimPrefix(repo, "https://github.com")
+	repo = strings.TrimPrefix(repo, "http://github.com")
+	repo = strings.TrimPrefix(repo, "/")
+	repo = strings.TrimSuffix(repo, "/")
+
+	parts := strings.Split(repo, "/")
+	if len(parts) != 2 {
+		panic(Sf("repo not valid: %q", repo))
+	}
+
+	owner := parts[0]
+	owner = strings.TrimSpace(owner)
+	if owner == "" {
+		panic(Sf("owner not valid: %q", owner))
+	}
+
+	repoName := parts[1]
+	repoName = strings.TrimSpace(repoName)
+	if repoName == "" {
+		panic(Sf("repoName not valid: %q", repoName))
+	}
+
+	return &Loader{
+		owner:    owner,
+		repoName: repoName,
+	}
+}
+
+const (
+	TYPE_PACKAGE    = "PACKAGE"
+	TYPE_REPOSITORY = "REPOSITORY"
+)
+
+func (ldr *Loader) Type(typ string) *Loader {
+	if !IsAnyOf(typ, TYPE_PACKAGE, TYPE_REPOSITORY) {
+		panic(Sf(
+			"Type not valid: %q; must be %q or %q",
+			typ,
+			TYPE_PACKAGE,
+			TYPE_REPOSITORY,
+		))
+	}
+	ldr.dependentType = typ
+	return ldr
+}
+
+func (ldr *Loader) SubPackage(pkg string) *Loader {
+	pkg = strings.TrimSpace(pkg)
+
+	ldr.subPackage = pkg
+	return ldr
+}
+func (ldr *Loader) DoWithCallback(callback func(dep string) bool) error {
+	if callback == nil {
+		return errors.New("callback is nil")
+	}
+	// TODO: validate necessary info.
+	if err := ldr.validateBasic(); err != nil {
+		return err
+	}
+
+	vals := url.Values{}
+	{
+		vals.Set("dependent_type", ldr.dependentType)
+	}
+
+	dst := Sf(
+		"https://github.com/%s/%s/network/dependents?%s",
+		ldr.owner,
+		ldr.repoName,
+		vals.Encode(),
+	)
+	doc, err := loadPage(dst)
+	if err != nil {
+		return err
+	}
+
+	// Check if the right subpackage is selected:
+	{
+		subs := extractSubPackages(doc)
+		if ldr.subPackage != "" {
+			is := subs.IsSelected(ldr.subPackage)
+			if !is {
+				sub := subs.ByName(ldr.subPackage)
+				if sub == nil {
+					return fmt.Errorf("Subpackage %q not found.", ldr.subPackage)
+				}
+
+				{
+					// Reload doc with the right subpackage:
+					doc, err = loadPage("https://github.com" + sub.URL)
+					if err != nil {
+						return err
+					}
+				}
+			}
+		}
+	}
+
+	// Process the first page:
+	for _, v := range extractDependents(doc) {
+		doContinue := callback(v)
+		if !doContinue {
+			return nil
+		}
+	}
+
+	nextPage := extractNextPage(doc)
+
+	for {
+		if nextPage == "" {
+			return nil
+		}
+		doc, err := loadPage(nextPage)
+		if err != nil {
+			return err
+		}
+		for _, v := range extractDependents(doc) {
+			doContinue := callback(v)
+			if !doContinue {
+				return nil
+			}
+		}
+		nextPage = extractNextPage(doc)
+	}
+
+	return nil
+}
+
+func (ldr *Loader) validateBasic() error {
+	if ldr.owner == "" {
+		return errors.New("owner not set")
+	}
+	if ldr.repoName == "" {
+		return errors.New("repoName not set")
+	}
+	if ldr.dependentType == "" {
+		return errors.New("dependentType not set")
+	}
+	return nil
+}
+
+type Info struct {
+	SubPackages SubPackageSlice
+	Counts      *Counts
+}
+
+type Counts struct {
+	Repositories int
+	Packages     int
+}
+
+func (ldr *Loader) GetInfo() (*Info, error) {
+	// TODO: validate necessary info.
+	if err := ldr.validateBasic(); err != nil {
+		return nil, err
+	}
+
+	vals := url.Values{}
+	{
+		vals.Set("dependent_type", ldr.dependentType)
+	}
+
+	dst := Sf(
+		"https://github.com/%s/%s/network/dependents?%s",
+		ldr.owner,
+		ldr.repoName,
+		vals.Encode(),
+	)
+	doc, err := loadPage(dst)
+	if err != nil {
+		return nil, err
+	}
+
+	info := new(Info)
+
+	// TODO: validate what has been extracted???
+	info.SubPackages = extractSubPackages(doc)
+
+	repoCount, packageCount := extractCounts(doc)
+	info.Counts = &Counts{
+		Repositories: repoCount,
+		Packages:     packageCount,
+	}
+
+	return info, nil
+}
+func loadPage(url string) (*goquery.Document, error) {
+	req := request.NewRequest(httpClient)
+	resp, err := req.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, formatRawResponseBodyError(resp)
+	}
+
+	reader, closer, err := resp.DecompressedReaderFromPool()
+	if err != nil {
+		return nil, fmt.Errorf("error while getting Reader: %s", err)
+	}
+	defer closer() //TODO
+	// Load the HTML document
+	return goquery.NewDocumentFromReader(reader)
+}
+func formatRawResponseBodyError(resp *request.Response) error {
+	// Get the body as text:
+	body, err := resp.Text()
+	if err != nil {
+		return fmt.Errorf("error while resp.Text: %w", err)
+	}
+	return fmt.Errorf(
+		"Status code: %v\nHeader:\n%s\nBody:\n\n %s",
+		resp.StatusCode,
+		Sq(resp.Header),
+		body,
+	)
+}
+
+func extractDependents(doc *goquery.Document) []string {
+	var dependants []string
 
 	// Find the review items
 	doc.Find("[data-repository-hovercards-enabled]").Each(func(i int, s *goquery.Selection) {
@@ -89,43 +408,118 @@ func getDependantRepos(reader io.Reader) []string {
 
 		if repositoryHrefOk {
 			trimmed := strings.TrimPrefix(repositoryHref, `/`)
-			rawDependants = append(rawDependants, trimmed)
+			dependants = append(dependants, trimmed)
 		}
+		// NOTE: only dependents that have a repository are extracted.
+		// Others are ignored.
 	})
-
-	next, ok := doc.Find(`[data-test-selector="pagination"]`).ChildrenFiltered("a").Attr("href")
-	if ok {
-		Ln("Next:", next)
-	}
-
-	return rawDependants
+	return dependants
 }
-func getDependantPackages(reader io.Reader) []string {
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(reader)
-	if err != nil {
-		log.Fatal(err)
+
+func extractNextPage(doc *goquery.Document) string {
+	next, ok := doc.Find(`[data-test-selector="pagination"]`).ChildrenFiltered("a").Last().Attr("href")
+	if ok {
+		return next
 	}
+	return ""
+}
 
-	var rawDependants []string
+type SubPackage struct {
+	Name     string
+	URL      string
+	Selected bool
+}
 
-	// Find the review items
-	doc.Find("[data-repository-hovercards-enabled]").Each(func(i int, s *goquery.Selection) {
+type SubPackageSlice []*SubPackage
+
+//
+func (sl SubPackageSlice) IsSelected(name string) bool {
+	for _, item := range sl {
+		if item.Name == name {
+			return item.Selected
+		}
+	}
+	return false
+}
+
+func (sl SubPackageSlice) ByName(name string) *SubPackage {
+	for _, item := range sl {
+		if item.Name == name {
+			return item
+		}
+	}
+	return nil
+}
+
+func extractSubPackages(doc *goquery.Document) SubPackageSlice {
+	res := make([]*SubPackage, 0)
+	packages := doc.Find(`div.select-menu-list`).ChildrenFiltered("a.select-menu-item")
+
+	packages.Each(func(i int, pkg *goquery.Selection) {
 		// For each item found, get the band and title
 
-		repository := s.ChildrenFiltered("[data-hovercard-type='repository']")
-		repositoryHref, repositoryHrefOk := repository.Attr("href")
-
-		if repositoryHrefOk {
-			trimmed := strings.TrimPrefix(repositoryHref, `/`)
-			rawDependants = append(rawDependants, trimmed)
+		pkgURL, ok := pkg.Attr("href")
+		if !ok {
+			return
 		}
+		nameNode := pkg.ChildrenFiltered("span.select-menu-item-text")
+		nameText := strings.TrimSpace(nameNode.Text())
+
+		sub := &SubPackage{
+			Name: nameText,
+			URL:  pkgURL,
+		}
+
+		isSelectedText, _ := pkg.Attr("aria-checked")
+		if isSelectedText == "true" {
+			sub.Selected = true
+		}
+
+		res = append(res, sub)
+	})
+	return res
+}
+
+func extractCounts(doc *goquery.Document) (repoCount int, packageCount int) {
+	counts := doc.Find(`div.table-list-header-toggle`).ChildrenFiltered("a")
+
+	counts.Each(func(i int, count *goquery.Selection) {
+		// For each item found, get the band and title
+
+		nameText := strings.TrimSpace(count.Text())
+
+		var netType string
+		processedString := countCleaner.ReplaceAllString(nameText, "")
+		if strings.Contains(nameText, "Repositor") {
+			netType = "REPOSITORY"
+
+			parsed, err := Atoi(processedString)
+			if err != nil {
+				panic(err)
+			}
+			repoCount = parsed
+		}
+		if strings.Contains(nameText, "Package") {
+			netType = "PACKAGE"
+
+			parsed, err := Atoi(processedString)
+			if err != nil {
+				panic(err)
+			}
+			packageCount = parsed
+		}
+
+		Sfln("Count %s: %q", netType, processedString)
 	})
 
-	next, ok := doc.Find(`[data-test-selector="pagination"]`).ChildrenFiltered("a").Attr("href")
-	if ok {
-		Ln("Next:", next)
+	return
+}
+
+func ExtractDependentsFromReader(reader io.Reader) ([]string, error) {
+	doc, err := goquery.NewDocumentFromReader(reader)
+	if err != nil {
+		return nil, err
 	}
 
-	return rawDependants
+	return extractDependents(doc), nil
 }
